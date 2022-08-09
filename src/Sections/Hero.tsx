96431ba8bca07a25272465a652dd6a9ae105be62
Maria Serykh <marie.serykh@gmail.com>
2022-08-10 00:20:27 +0200

    fix: fix hero section structure

--- src/Sections/Hero.tsx
+++ src/Sections/Hero.tsx
@@ -1,13 +1,17 @@
+import Section from '../components/Section';
+
 type HeroProps = {
   children?: React.ReactNode;
 };
 
 const Hero = (props: HeroProps) => {
   return (
-    <p className="text-6xl font-bold">
-      Helps to develop modular, independent, portable, and scalable an
-      applications.
-    </p>
+    <Section>
+      <p className="text-6xl font-bold">
+        Helps to develop modular, independent, portable, and scalable an
+        applications.
+      </p>
+    </Section>
   );
 };
 
