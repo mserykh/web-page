325c7cd458c00075db2443b43912b16773e0c825
Maria Serykh <marie.serykh@gmail.com>
2022-08-09 17:02:23 +0200

    build: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <h1>12 factors application
-    </h1>
-  );
+  return <h1>12 factors application</h1>;
 }
 
 export default App;
