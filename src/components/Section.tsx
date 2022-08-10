224e782ab10e02bbcf2ab7550880115f63a2b0d7
Maria Serykh <marie.serykh@gmail.com>
2022-08-10 04:13:37 +0200

    refactor: adjust section styles

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -7,7 +7,9 @@ type SectionProps = {
 const Section = (props: SectionProps) => {
   return (
     <section id={props.id} className="container mx-auto px-4 mb-10">
-      <h2>{props.title}</h2>
+      <h2 className="mb-4 text-3xl font-bold uppercase tracking-wide">
+        {props.title}
+      </h2>
       {props.children}
     </section>
   );
