7a67af9932c0fbe4e8ed051959863567c9c40e2e
Maria Serykh <marie.serykh@gmail.com>
2022-08-11 20:44:12 +0200

    feat: add effect on scroll to `About` section

--- src/Sections/About.tsx
+++ src/Sections/About.tsx
@@ -1,15 +1,10 @@
+import AboutCard from '../components/AboutCard';
 import Section from '../layouts/Section';
 
 const About = () => {
   return (
     <Section id="about" title="What Is the Twelve-Factor Methodology?">
-      <p className="text-lg">
-        The twelve-factor methodology is a set of twelve best practices to
-        develop applications developed to run as a service. This was originally
-        drafted by Heroku for applications deployed as services on their cloud
-        platform, back in 2011. Over time, this has proved to be generic enough
-        for any software-as-a-service (SaaS) development.
-      </p>
+      <AboutCard />
     </Section>
   );
 };
