363736098fb3681f22a1557a4d7b6f6bb7c1bc98
Maria Serykh <marie.serykh@gmail.com>
2022-08-12 07:11:54 +0200

    fix: fix typo in logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <a href="#" className="flex items-center">
       <img src={logo} alt="logo" />
-      <h1 className="ml-2 text-3xl font-black uppercase">12 factor apps</h1>
+      <h1 className="ml-2 text-3xl font-black uppercase">12-factor apps</h1>
     </a>
   );
 };
