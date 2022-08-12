870a8007a5aeb584bd46e250260fef09e0c80871
Maria Serykh <marie.serykh@gmail.com>
2022-08-12 08:43:45 +0200

    feat: add links and developer name to the footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,10 +1,19 @@
 const Footer: React.FC = () => {
+  const year = new Date().getFullYear();
   return (
     <footer className="container mx-auto px-10 py-6">
       <div className="flex justify-between items-center">
-        <span>© 2022</span>
-        <a href="https://github.com/mserykh">mserykh</a>
-        <span>Made for</span>
+        <span>{`© ${year}`}</span>
+        <span>Made by Maria Serykh</span>
+        <a
+          href="https://github.com/mserykh"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:text-lime-400 active:text-fuchsia-500"
+          aria-label="Go to the github account of the developer"
+        >
+          github
+        </a>
       </div>
     </footer>
   );
